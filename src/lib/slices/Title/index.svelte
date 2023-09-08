1d9129df7bf121dc3d83aa42a7f9940b2d429b4e
SanHont <dehontsander@gmail.com>
2023-09-08 14:32:41 +0200

    First Update

---
+++ src/lib/slices/Title/index.svelte
@@ -0,0 +1,20 @@
+<script>
+  /** @type {import("@prismicio/client").Content.TitleSlice} */
+  export let slice;
+</script>
+
+<section
+  data-slice-type={slice.slice_type}
+  data-slice-variation={slice.variation}
+>
+ 
+<h1>{slice.primary.title[0].text}</h1>
+
+</section>
+
+<style>
+  h1{
+    font-size: 2rem;
+    font-family: 'Courier New', Courier, monospace;
+  }
+</style>
